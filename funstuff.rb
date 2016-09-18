80e91ac4751bdef8ea502de2dc6f92d5d3426b3b
Lars Holmberg <lars.holmberg@klarna.com>
2016-09-18 18:51:18 +0200

    add fe wrapper method

--- funstuff.rb
+++ funstuff.rb
@@ -4,13 +4,17 @@ require 'yaml'
 ENV['PATH'] = ENV['PATH'] + ':.'
 EXECUTE = ENV.fetch 'EXECUTE', false
 
+def fe(*args)
+  $driver.find_element(*args)
+end
+
 $driver = Selenium::WebDriver.for :chrome
 
 # at_exit { $driver.quit }
 
 $driver.navigate.to ENV['FUNSTUFF']
 
-wait = Selenium::WebDriver::Wait.new(timeout: 5)
+wait = Selenium::WebDriver::Wait.new(timeout: 3)
 
 YAML.load_file("cookies.yml").each do |cookie|
   $driver.manage.add_cookie cookie
@@ -19,13 +23,13 @@ end
 $driver.navigate.to ENV['FUNSTUFF']
 
 begin
-  wait.until { /totalPortfolio/.match($driver.page_source) } # Not logged in
+  wait.until { fe(class: "search-control__control") }
 rescue
   $driver.navigate.to ENV['FUNSTUFF_LOGIN']
-  element = $driver.find_element(:name, 'j_username')
+  element = fe(:name, 'j_username')
   sleep 1
   element.send_keys ENV['FARG']
-  element = $driver.find_element(:name, 'j_password')
+  element = fe(:name, 'j_password')
   sleep 1
   element.send_keys ENV['GARF']
   sleep 1
@@ -33,27 +37,32 @@ rescue
   File.open("cookies.yml", 'w') do |f|
     f.write YAML.dump($driver.manage.all_cookies)
   end
+  wait.until { fe(class: "search-control__control") }
 end
 
-def order(action, instrument, amount, limit)
-  $driver.find_element(class: "search-control__control").click
-  $driver.find_element(class: "search-control__control").clear
-  $driver.find_element(class: "search-control__control").send_keys instrument
+
+def order(instrument, amount, limit)
+  action = amount < 0 ? 'sell' : 'buy'
+  fe(class: "search-control__control").click
+  fe(class: "search-control__control").clear
+  fe(class: "search-control__control").send_keys instrument
   sleep 1
-  $driver.find_element(class: "quick-order-search__products-list-item-title").click
+  fe(class: "quick-order-search__products-list-item-title").click
   sleep 1
-  $driver.find_element(css: "[data-action='#{action}']").click
-  $driver.find_element(css: "[data-ng-model='orderData.number']").send_keys amount.to_s
-  $driver.find_element(css: "[data-ng-model='orderData.limit']").send_keys limit.to_s
+  fe(css: "[data-action='#{action}']").click
+  fe(css: "[data-ng-model='orderData.number']").send_keys amount.to_s
+  fe(css: "[data-ng-model='orderData.limit']").send_keys limit.to_s
+
+  puts "#{instrument},#{amount},#{limit},#{EXECUTE}"
 
   if EXECUTE == 'true'
-    $driver.find_element(class: "order-products-form__button").click
-    $driver.find_element(class: "order-modal__footer-button_cancel").click
-    $driver.find_element(class: "order-modal__footer-button_confirm").click
+    fe(class: "order-products-form__button").click
+    fe(class: "order-modal__footer-button_cancel").click
+    fe(class: "order-modal__footer-button_confirm").click
   end
-
-  $driver.find_element(class: "header__navigation-logo").click
+  sleep 5
+  fe(class: "header__navigation-logo").click
 end
 
-order 'buy', 'HMB', 10, 250
-order 'buy', 'HMB', 10, 249
+order 'HMB', 10, 250.1
+order 'HMB', -10, 249.5
